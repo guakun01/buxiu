08adada284ed15ecd9d98a5ff19b3cf61a9b3307
guakun01 <guakun01@gmail.com>
2023-03-19 17:29:57 +0800

    feat: 添加订单监控

--- src/main/java/com/github/guakun01/buxiu/BuXiuApplication.java
+++ src/main/java/com/github/guakun01/buxiu/BuXiuApplication.java
@@ -1,13 +1,49 @@
 package com.github.guakun01.buxiu;
 
+import com.github.guakun01.buxiu.actuator.SalesMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Random;
 
 @SpringBootApplication
+@EnableScheduling
 public class BuXiuApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(BuXiuApplication.class);
+
+	private Random random = new Random();
+
+	@Autowired
+	private SalesMetrics salesMetrics;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuXiuApplication.class, args);
+	}
+
+	@Bean
+	public MeterRegistry customMeterRegistry() {
+		CompositeMeterRegistry meterRegistry = new CompositeMeterRegistry();
+		meterRegistry.add(new SimpleMeterRegistry());
+		meterRegistry.add(new LoggingMeterRegistry());
+		return meterRegistry;
+	}
+
+	@Scheduled(fixedRate = 3000, initialDelay = 500)
+	public void periodcallyMakeAnOrder() {
+		int amount = random.nextInt(100);
+		salesMetrics.makeNewOrder(amount);
+		logger.info("有顾客下单了，订单金额 {} 元。", amount);
 	}
 
 }
---
+++ src/main/java/com/github/guakun01/buxiu/actuator/SalesMetrics.java
@@ -0,0 +1,47 @@
+package com.github.guakun01.buxiu.actuator;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class SalesMetrics implements MeterBinder {
+    /**
+     * 订单金额
+     */
+    private Counter orderCount;
+
+    /**
+     * 总订单金额
+     */
+    private Counter totalAmount;
+
+    /**
+     * 95 分位的订单情况
+     */
+    private DistributionSummary orderSummary;
+
+    /**
+     * 客单价
+     */
+    private AtomicInteger averageAmount = new AtomicInteger();
+
+    @Override
+    public void bindTo(MeterRegistry registry) {
+        this.orderCount = registry.counter("order.count", "direction", "income");
+        this.totalAmount = registry.counter("order.amount.sum", "direction", "income");
+        this.orderSummary = registry.summary("order.summary", "direction", "income");
+        registry.gauge("order.amount.average", averageAmount);
+    }
+
+    public void makeNewOrder(int amount) {
+        orderCount.increment();
+        totalAmount.increment(amount);
+        orderSummary.record(amount);
+        averageAmount.set((int) orderSummary.mean());
+    }
+}
